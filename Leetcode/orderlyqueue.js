b29a0c420beec337d0b77833c1ef83b5a69b8d57
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-11-08 09:30:18 +0530

    sum added

---
+++ Leetcode/orderlyqueue.js
@@ -0,0 +1,13 @@
+const orderlyQueue = function (s, k) {
+    if (k === 1) {
+      let min = s;
+      for (let i = 1; i < s.length; i++) {
+        const sub = s.substring(i) + s.substring(0, i);
+        if (sub < min) {
+          min = sub;
+        }
+      }
+      return min;
+    }
+    return s.split('').sort().join('');
+  };
